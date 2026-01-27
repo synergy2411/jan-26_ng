029fbf1724da0c9de7c37b200ec70cfa797e807a
Sumit Khandelwal <synergy2411@outlook.com>
2026-01-27 10:26:44 +0530

    error messages added

--- frontend-two/src/app/components/auth/register/register.component.ts
+++ frontend-two/src/app/components/auth/register/register.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,16 +12,23 @@ import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
   styleUrl: './register.component.css',
 })
 export class RegisterComponent {
-  username = new FormControl();
-  password = new FormControl();
+  // uname = new FormControl();
+  // password = new FormControl();
 
   registerForm: FormGroup;
 
   constructor(private fb: FormBuilder) {
     this.registerForm = this.fb.group({
-      username: this.username,
-      password: this.password,
+      username: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
     });
+  }
+
+  get username() {
+    return this.registerForm.get('username') as FormControl;
   }
 
   onSubmit() {
